1262831ad825fd92f4fa1e0e20eac976a80e01dc
LabanAllan <labanallan254@gmail.com>
2023-03-30 15:22:32 -0700

    . Print list

--- lists.h
+++ lists.h
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
 
-#define "list.h"
-#ifndef "list.h"
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
